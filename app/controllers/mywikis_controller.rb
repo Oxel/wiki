ae70700a03f2496babb8e2b1b457a8fa7aa44246
Charlie Gaines <beaugaines@gmail.com>
2014-10-31 14:46:02 -0400

    add user to mywiki params

--- app/controllers/mywikis_controller.rb
+++ app/controllers/mywikis_controller.rb
@@ -7,9 +7,17 @@ class MywikisController < ApplicationController
   end
 
   def create
-    @mywiki
-    
+    @mywiki = current_user.mywikis.build(mywiki_params)
+    if @mywiki.save
+      redirect_to root_path, notice: 'Wiki added'
+    else
+      flash[:error] = 'Wiki creation failed'
+      render :new
+    end
   end
-  
-  
+
+  private
+  def mywiki_params
+    params.require(:mywiki).permit(:user, :title, :body)
+  end
 end
