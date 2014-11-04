cea12340c0a471216ecf5645fb807ddb8ea57217
Louis Morin <louismorin@Louiss-MacBook-Air.local>
2014-11-04 10:34:05 -0500

    Added wiki edit function

--- app/controllers/mywikis_controller.rb
+++ app/controllers/mywikis_controller.rb
@@ -5,12 +5,16 @@ class MywikisController < ApplicationController
 		@mywiki = Mywiki.new
 	end
 
+  def show
+    redirect_to root_path
+  end
+
   def index
   	@mywikis = current_user.mywikis
   end
 
   def create
-  	@mywikis = current_user.mywikis.build(mywiki_params)
+  	@mywiki = current_user.mywikis.build(mywiki_params)
   	if @mywiki.save
   	 redirect_to root_path, notice: 'Wiki added'
     else
@@ -18,6 +22,22 @@ class MywikisController < ApplicationController
       render :new
     end
 	end
+
+  def edit
+    @mywiki = Mywiki.find(params[:id])
+  end
+
+  def update
+    @mywiki = Mywiki.find(params[:id])
+    if @mywiki.update(mywiki_params)
+      flash[:notice] = "Wiki has been updated."
+      redirect_to root_path
+    else
+      flash[:alert] = "Wiki has not been updated."
+      render "edit"
+    end
+  end
+
 
   def destroy
     @mywiki = Mywiki.destroy(params[:id])
