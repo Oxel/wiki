fab1fcc6fae33da2c1070dca3a1dfc106a14b445
Charlie Gaines <beaugaines@gmail.com>
2014-11-14 10:20:42 -0500

    merge conflict

--- app/models/mywiki.rb
+++ app/models/mywiki.rb
@@ -1,11 +1,7 @@
 class Mywiki < ActiveRecord::Base
-<<<<<<< HEAD
-  belongs_to :user
-=======
 
 	belongs_to :user
 
 	scope :visible_to, -> (user) { user ? all: where(public: true) }
 	
->>>>>>> fa6d2e8fa259ef4ad174ddbce38a7d7c3a566b90
 end
